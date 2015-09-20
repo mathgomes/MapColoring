47492eaf983359a74d5de1f24688bd7c2b6e364b
Matheus Gomes <mtgomes6644@gmail.com>
2015-09-20 19:47:00 -0300

    Entrada é lida

--- mapColoring.cpp
+++ mapColoring.cpp
@@ -18,51 +18,126 @@ bool isCommaOrSpaceOrDot(char c) {
         return false;
     }
 }
-bool isInVector(string a, vector<pair<string,int> > &states) {
+pair<string,int> searchInVector(pair<string,int> state, vector<pair<string,int> > &states, int *statesRead) {
 
     for( unsigned int j = 0; j < states.size(); j++) {
 
-        if(get<0>(states[j]) == a) {
-            return true;
+        if(get<0>(states[j]) == get<0>(state)) {
+            cout << "Estado "<<  get<0>(state) << " ja existe, retorne\n";
+            return states[j];
         }
     }
-    return false;
+    get<1>(state) = states.size();
+    states.push_back(state);
+    cout << "Estado "<<  get<0>(state) << " nao existe " << "tamanho do vetor " << states.size() << " indice do vertice " << get<1>(state) << "\n";
+    *statesRead = *statesRead + 1;
+    return state;
 }
+bool** allocateGraph(int vertexNum) {
+    bool **Graph = new bool*[vertexNum];
+    for (int i = 0; i < vertexNum ; ++i) {
+        Graph[i] = new bool[vertexNum];
+    }
+    for( int i = 0; i < vertexNum; i ++) {
+        for( int j = 0; j < vertexNum; j ++) {
+            Graph[i][j] = false;
+        }
+    }
+    return Graph;
+}
+void addEdge(bool **Graph, int source, int destiny) {
+
+    Graph[source][destiny] = true;
+    Graph[destiny][source] = true;
+}
+void printfGraph(bool **Graph, int vertexNum) {
+    cout << "\n";
+    for (int i = 0; i < vertexNum; ++i) {
+        for (int j = 0; j < vertexNum; ++j) {
+
+           cout << Graph[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
 int main() {
 
 
     int countriesNumber;
     char heuristic;
-    char c;
     cin >> countriesNumber;
     cin >> heuristic;
 
-    // ou usar stringstream
     string line;
-    string mainState;
+    pair<string,int> mainState;
     pair<string,int> neighbour;
-    vector < pair < string, int > > neighbourStates;
-    int i = 0;
+    vector < pair < string, int > > States;
+    stringstream ss1;
+    stringstream ss2;
+    string neighbourLine;
+    int statesRead = 0;
+
+    bool **Country = allocateGraph(countriesNumber);
+
     cin.ignore();
-    for ( int j =0; j < countriesNumber; j ++) {
+    while( statesRead < countriesNumber ) {
 
         getline(cin,line);
-        stringstream ss(line);
-        getline(ss, mainState, ':');
+        ss1 << line;
+        getline(ss1, get<0>(mainState), ':');
+        get<0>(mainState).erase(
+            remove_if(get<0>(mainState).begin(),get<0>(mainState).end(), &isCommaOrSpaceOrDot),get<0>(mainState).end()
+        );
+        mainState = searchInVector(mainState,States,&statesRead);
 
-        while(getline(ss, get<0>(neighbour), ',')) {
-            get<0>(neighbour).erase(
-                remove_if(get<0>(neighbour).begin(),get<0>(neighbour).end(), &isCommaOrSpaceOrDot),get<0>(neighbour).end()
-            );
-            if(!isInVector(get<0>(neighbour),neighbourStates)) {
-                get<1>(neighbour) = neighbourStates.size();
-                neighbourStates.push_back(neighbour);
+        while (getline(ss1, neighbourLine, '.')) {
+
+            ss2 << neighbourLine;
+            while(getline(ss2, get<0>(neighbour), ',')) {
+
+                get<0>(neighbour).erase(
+                    remove_if(get<0>(neighbour).begin(),get<0>(neighbour).end(), &isCommaOrSpaceOrDot),get<0>(neighbour).end()
+                );
+                neighbour = searchInVector(neighbour,States,&statesRead);
+                cout << "Adicionar aresta entre " << get<0>(mainState) << " " << get<1>(mainState) << "   " << get<0>(neighbour) << " " << get<1>(neighbour) << "\n";
+                addEdge(Country,get<1>(mainState),get<1>(neighbour));
             }
+            ss2.clear();
         }
-    }
-    for ( unsigned int i =0; i< neighbourStates.size(); i ++) {
-        cout << "\n" << get<0>(neighbourStates[i]) << ", " << get<1>(neighbourStates[i]) << "\n";
+        ss1.clear();
+
     }
 
+
+    for ( unsigned int i = 0; i < States.size(); i ++) {
+        cout << "\n" << get<0>(States[i]) << ", " << get<1>(States[i]) << "\n";
+    }
+    printfGraph(Country,countriesNumber);
+
+
+    switch(heuristic) {
+
+    case 'a':
+        // backtracking comum
+
+        break;
+    case 'b':
+
+        // backtracking + forward checking
+        break;
+    case 'c':
+        // backtracking + fc + mrv
+
+        break;
+    case 'd':
+        // bt + fc + mrv + grau
+
+        break;
+
+    }
+
+
+    system("PAUSE");
     return 0;
 }
